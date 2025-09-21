31407f5c15a8417ab9dc4253e1e7158d5a79914a
Максим Польгуев <polguevmi@digtp.com>
2025-09-21 17:57:56 +0300

    del rubbish

--- routers/feature_extraction.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-from core.templates import templates
-
-router = APIRouter()
-
-
-@router.get("/processing")
-def catalog(request: Request):
-    return templates.TemplateResponse("processing.html", {"request": request})
