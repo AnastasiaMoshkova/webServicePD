6656ec3de6673bfec0234f8fc7f4f4c3c0f4b8c3
Максим Польгуев <polguevmi@digtp.com>
2025-09-23 23:03:18 +0300

    routes in main

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from routers import home, feature_extraction, hand_tracking, files_processing
+from routers import tremor, hand_tracking, mimic, gait, voice
 
 app = FastAPI(limit_max_body_size=3 * 1024 * 1024 * 1024)  # 1 ГБ
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Подключаем роуты
-app.include_router(home.router)
-app.include_router(feature_extraction.router)
+app.include_router(tremor.router)
+app.include_router(mimic.router)
+app.include_router(gait.router)
+app.include_router(voice.router)
 app.include_router(hand_tracking.router)
-app.include_router(files_processing.router)
