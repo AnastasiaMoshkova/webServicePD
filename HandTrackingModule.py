dcdb1de6c697f158e2b538b1a747b8627c7e0c07
Максим Польгуев <polguevmi@digtp.com>
2025-09-23 23:04:41 +0300

    handtrcmodule minor changes

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,7 +1,9 @@
 import cv2
 import mediapipe as mp
 import time
-class handDetector():
+
+
+class handDetector:
     def __init__(self, mode=False, maxHands=2, modelComplex=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
@@ -9,9 +11,12 @@ class handDetector():
         self.detectionCon = detectionCon
         self.trackCon = trackCon
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(
+            self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon
+        )
 
         self.mpDraw = mp.solutions.drawing_utils
+
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
@@ -19,9 +24,9 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLms,
-                                               self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
+
     def findPosition(self, img, handNo=0, draw=True):
         lmList = []
         if self.results.multi_hand_landmarks:
@@ -32,9 +37,9 @@ class handDetector():
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 # print(id, cx, cy)
                 lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
         return lmList
+
+
 def main():
     pTime = 0
     cTime = 0
@@ -49,10 +54,11 @@ def main():
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.imshow("Image", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+
+
 if __name__ == "__main__":
     main()
